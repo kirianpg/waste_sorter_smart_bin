923681f35be1f4cedc5f97aeedb866c7ce5d677a
susischaller <susanne.schaller95@gmx.at>
2024-03-05 18:23:04 +0100

    first version UI

---
+++ app/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.markdown("""# This is your new SMART BIN!
+## It will take care of your waste management by classifying your waste automatically.
+The waste classifications in our MVP are:""")
