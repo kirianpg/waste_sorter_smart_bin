88bd14af96aead50b3a87741aa1294dd6be267e1
Michelange MBETIBANGA <michelangembetibanga@yahoo.fr>
2024-03-07 20:12:32 +0100

    Adding new functions in make file for local setup

---
+++ project_waste_sorter/params.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+DATA_SIZE = "all" # ["1k", "200k", "all"]
+CHUNK_SIZE = 100000
+GCP_PROJECT = "wagon-bootcamp-414210" # TO COMPLETE
+GCP_PROJECT_WAGON = "wagon-public-datasets" # TO COMPLETE
+BQ_DATASET = "taxifare" # TO COMPLETE
+BQ_REGION = "EU"
+MODEL_TARGET = "local"
+##################  CONSTANTS  #####################
+LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), ".lewagon", "waste_smart_sorter_bin", "data")
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), ".lewagon", "waste_smart_sorter_bin", "training_outputs")
+COLUMN_NAMES_RAW = ['fare_amount','pickup_datetime', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'passenger_count'] # TO MODIFY
+
+# TO MODIFY
+DTYPES_RAW = {
+    "fare_amount": "float32",
+    "pickup_datetime": "datetime64[ns, UTC]",
+    "pickup_longitude": "float32",
+    "pickup_latitude": "float32",
+    "dropoff_longitude": "float32",
+    "dropoff_latitude": "float32",
+    "passenger_count": "int16"
+}
+
+DTYPES_PROCESSED = np.float32
