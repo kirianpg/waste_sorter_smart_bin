45148379eafd2a530a8469d36a57630df1290074
susischaller <susanne.schaller95@gmx.at>
2024-03-17 12:51:30 +0100

    changes frontend & api file

--- project_waste_sorter/api/api_file.py
+++ project_waste_sorter/api/api_file.py
@@ -1,20 +1,45 @@
 from fastapi import FastAPI, File, UploadFile
-from project_waste_sorter.frontend.app.app import pred_streamlit
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
 from project_waste_sorter.ml_logic.registry import *
+from google.cloud import storage
+from io import BytesIO
+from PIL import Image
 
-api = FastAPI()
+app = FastAPI()
 
-api.state.model = load_model()
+model = load_model()  # Load the model at startup
 
-# define a root `/` endpoint
-@api.get("/")
+@app.get("/")
 def index():
-    return {"ok": "API connected"}
+    return {"message": "API connected"}
 
-# prediction endpoint that calls streamlit function (depends on main file for model)
-@api.post("/prediction")
-async def pred(img: UploadFile = File(...)):
-    img = img.file.read()
-    predicted_class = pred_streamlit(img, api.state.model)
+@app.post("/predict/")
+async def classify_image(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        uploaded_image = Image.open(BytesIO(contents))
+        # Resize to 224 x 224
+        uploaded_image = uploaded_image.resize((224, 224))
 
-    return predicted_class
+        # Convert the image pixels to a numpy array
+        image = img_to_array(uploaded_image)
+
+        # Reshape data for the model
+        image = image.reshape((1, 224, 224, 3))
+
+        # Prepare the image for the VGG model
+        image = preprocess_input(image)
+
+        # Run prediction
+        model_prediction = model.predict(image)
+
+        # Format the result
+        INDEX_TO_CATEGORIES = {v: k for k, v in CATEGORIES_MAP.items()}
+        predictions_with_categories = [(INDEX_TO_CATEGORIES[i], prob) for i, prob in enumerate(model_prediction[0])]
+        predictions_with_categories.sort(key=lambda x: x[1], reverse=True)
+        best_prediction = predictions_with_categories[0]
+
+        return best_prediction
+    except Exception as e:
+        return {"error": str(e)}
